98455597f808db5f505c9ebb5113397e6b386aac
Jon Zagorski <JonZagorski@gmail.com>
2021-01-09 04:17:09 -0500

    gave up on env

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template
 import os
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
-print(os.environ['APP_SETTINGS'])
+
 @app.route("/")
 def home():
     return render_template("home.html")
