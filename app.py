0ae538734f8faf7dbbbe3ed62ea468ed1785ea92
Jon Zagorski <JonZagorski@gmail.com>
2021-01-11 15:49:50 -0500

    added index.html

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 @app.route("/about")
 def about():
