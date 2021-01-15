fe25f52463d862f2a6f4bc5643d92234a9f272d9
Jon Zagorski <JonZagorski@gmail.com>
2021-01-15 16:57:00 -0500

    adding everything to heroku

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ db = SQLAlchemy(app)
 
 
 
-con = psycopg2.connect(insert database url )
+con = psycopg2.connect("postgresql://postgres:jh0njr&p3nny@database-1.c84rdrfagztk.us-east-1.rds.amazonaws.com/postgres")
 
 cursor = con.cursor()
 
