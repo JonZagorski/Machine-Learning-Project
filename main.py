14338cb27404d30f831e6c341e54327f7daa4a14
Brendan Rhoads <brendan.rhoads@gmail.com>
2021-01-21 17:10:22 -0500

    final commit before presentation 1/21

--- main.py
+++ main.py
@@ -1,36 +1,47 @@
-import models
-import models2
-import features
-import output
-import finance
-import plot 
+# import models
+# import models2
+# import features
+# import output
+# import finance
+# import plot 
+import model_picking
 import pandas as pd
 
 
-symbols = ['WMB','PSX', 'FANG', 'COP', 'XEC', 'PXD', 'EPD', 'GLNG', 'NOG', 'DVN']
+#symbols = ['WMB','PSX', 'FANG', 'COP', 'XEC', 'PXD', 'EPD', 'GLNG', 'NOG', 'DVN']
 
-#symbol = 'WMB'
+symbol = 'WMB'
 
-#models2.get_historical(symbol)
+model_picking.get_historical(symbol)
 
+# for symbol in symbols: #runs the models on every symbol.
 
-#symbols = sys.argv[1:] #loads symbols from the command line.
-for symbol in symbols: #runs the models on every symbol.
-    #models2.get_historical(symbol)
-    #print(score)
-    finance.get_historical(symbol)
-    df = features.loadDataset(symbol)
-    model_prediction =  models.train(df)
-    df["Ticker"] = symbol
-    # Converting the index as date
-    df.index = pd.to_datetime(df.index)
+#     #run get_historical to get data on each ticker
+#     finance.get_historical(symbol)
 
-    # Extracting hour & minute
-    df['hour'] = df.index.hour
-    df['minute'] = df.index.minute
+#     #load dataset in as a dataframe
+#     df = features.loadDataset(symbol)
 
-    final_df = df.merge(model_prediction, on = ["Date"])
-    final = output.output(final_df, symbol)
-    #plot.dataset_plot(df, symbol)
-    connection = output.pg_connection(final_df)
-    #print(f"Scores of models on the dataset {symbol} (Linear, Poly, RBF, Sigmoid, RFC, AdaBoost, VotingClassifier): {[score * 100 for score in scores_models]}")
+#     #train the dataframe using train_test split and run the dataframe through
+#     #the Decision Tree Regressor
+#     model_prediction =  models.train(df)
+
+#     #add ticker name to each dataframe
+#     df["Ticker"] = symbol
+
+#     # Converting the index as date
+#     df.index = pd.to_datetime(df.index)
+
+#     # Extracting hour & minute
+#     df['hour'] = df.index.hour
+#     df['minute'] = df.index.minute
+
+#     #merge both dataframes with predictions and regular data
+#     final_df = df.merge(model_prediction, on = ["Date"])
+
+#     #output as CSV
+#     final = output.output(final_df, symbol)
+#     #plot.dataset_plot(df, symbol)
+
+#     #connect dataframe to PostgreSQL
+#     connection = output.pg_connection(final_df)
--- models.py
+++ models.py
@@ -12,22 +12,19 @@ from sklearn.tree import DecisionTreeRegressor
 
 
 def train(df):
-    '''This function trains the data on 4 different SVC model kernels:
-    1. Linear Kernel
-    2. Polynomial Kernel
-    3. Radial Basis Function Kernel
-    4. Sigmoid Kernel
-    The RFC model is also implemented. 
-    
-    The hyperparameters are set default in each case.
-    The score of the model on the Dev/Test set is returned to the main script.
-    '''
+
     X, y = features.addFeatures(df)
 
     X_train, X_test, y_train, y_test = features.splitDataset(X, y)
-    X_train, X_test = features.featureScaling(X_train, X_test)
-
     
+    X_train, X_test = features.featureScaling(X_train, X_test)
+    
+    model_cart = DecisionTreeRegressor()
+    model_cart.fit(X_train, y_train)
+    cart_predictions = model_cart.predict(X_test)
+
+    df=pd.DataFrame({'Actual':y_test, 'Predicted':cart_predictions})
+
     #model_slinear = svm.SVC(kernel='linear')
 
     # Create the GridSearch estimator along with a parameter object containing the values to adjust
@@ -51,12 +48,6 @@ def train(df):
     # model_spoly = svm.SVC(kernel='poly')
     # model_spoly.fit(X_train, y_train)
     # score_spoly = model_spoly.score(X_test, y_test)
-    
-    model_cart = DecisionTreeRegressor()
-    model_cart.fit(X_train, y_train)
-    cart_predictions = model_cart.predict(X_test)
-
-    df=pd.DataFrame({'Actual':y_test, 'Predicted':cart_predictions})
     
     #print(cart_predictions)
 
